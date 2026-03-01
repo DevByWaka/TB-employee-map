f1a81ffbb124a96b5394070f7d79e253256ee8f8
AKAW1210 <waka1210.games@gmail.com>
2026-03-02 01:27:40 +0900

    init

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/TB-employee-map/',   // ← ここをGitHubのリポジトリ名に変更
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
